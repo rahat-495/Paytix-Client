78e3fc1ebed1d61cb87b48f604c7962674f4c0aa
Md Rahat <md8702687@gmail.com>
2024-07-14 21:56:33 +0600

    addclass page started editing !

--- src/Pages/AddClass/AddClass.jsx
+++ src/Pages/AddClass/AddClass.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+
+import React from "react";
 
 const AddClass = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="min-h-[70vh]">
+
+      <h1 className="play text-center my-16 text-4xl text-white">
+        Add A School
+      </h1>
+
+    </div>
+  );
 };
 
 export default AddClass;
